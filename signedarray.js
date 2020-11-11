fd17b03af4c5b6deef3aa7c419c9188b5671c2a8
ron <rinbar70@gmail.com>
2020-11-11 20:59:07 +0200

    Solve.

---
+++ signedarray.js
@@ -0,0 +1,21 @@
+const a = [-4, -2, 3, -11, 7, 5, -4, 1, 2, -3];
+
+let p = 0, q = a.length - 1;
+
+while (p < q) {
+    if (a[p] < 0) {
+        p++;
+    } 
+    else if (a[q] >= 0) {
+        q--;
+    }
+    else {
+        const n = a[p];
+        a[p] = a[q];
+        a[q] = n;
+        p++;
+        q--;
+    }
+}
+
+console.log(a);
